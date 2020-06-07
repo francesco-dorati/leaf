94ee32f4dae0c5b5d686aba55ef83b42bbcbd2f6
Francesco Dorati <fd.francescodorati@gmail.com>
2020-06-07 12:52:42 +0200

    username/email length-regexp

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -6,59 +6,79 @@ const username = $('#username');
 const email = $('#email');
 const password = $('#password');
 const repeatPassword = $('#repeat_password');
+const usernameInvalidFeedback = $('#username-form .invalid-feedback');
+const emailInvalidFeedback = $('#email-form .invalid-feedback');
 const submit = $('#submit');
 let usernameValid = true;
 let emailValid = true;
 
 $(() => {
-/* USERNAME */ // ADD LENGTH CHECK ----------------------------------------------------------------
+/* USERNAME */
   usernameForm.on('focusout', () => {
+    // Length
     if (username.val()) {
-      $.ajax({
-        url: '/authentication/username',
-        data: {
-          username: username.val(),
-        },
-        method: 'POST',
-        success: (valid) => {
-          if (valid) {
-            usernameValid = true;
-            username.addClass('is-valid');
-            if (emailValid) submit.attr('disabled', false);
-          } else {
-            usernameValid = false;
-            username.addClass('is-invalid');
-            submit.attr('disabled', true);
-          }
-        },
-      });
+      if (username.val().length > 3) {
+        $.ajax({
+          url: '/authentication/username',
+          data: {
+            username: username.val(),
+          },
+          method: 'POST',
+          success: (valid) => {
+            if (valid) {
+              usernameValid = true;
+              username.addClass('is-valid');
+              if (emailValid) submit.attr('disabled', false);
+            } else {
+              usernameValid = false;
+              username.addClass('is-invalid');
+              usernameInvalidFeedback.text('Username already taken.');
+              submit.attr('disabled', true);
+            }
+          },
+        });
+      } else {
+        usernameValid = false;
+        username.addClass('is-invalid');
+        usernameInvalidFeedback.text('4 characters minimum.');
+        submit.attr('disabled', true);
+      }
     }
   });
   usernameForm.on('keyup', () => {
     username.removeClass('is-invalid').removeClass('is-valid');
   });
 
-  /* EMAIL */ // ADD EMAIL CHECK --> REGEX
+  /* EMAIL */
   emailForm.on('focusout', () => {
+    // reg ex
     if (email.val()) {
-      $.ajax({
-        url: '/authentication/email',
-        data: {
-          email: email.val(),
-        },
-        method: 'POST',
-        success: (valid) => {
-          if (valid) {
-            emailValid = true;
-            email.removeClass('is-invalid');
-            if (usernameValid) submit.attr('disabled', false);
-          } else {
-            emailValid = false;
-            email.addClass('is-invalid');
-            submit.attr('disabled', true);
-          }
-        },
-      });
+      const regEx = /^\S+@\w+\.+\w+$/;
+      if (regEx.test(email.val())) {
+        $.ajax({
+          url: '/authentication/email',
+          data: {
+            email: email.val(),
+          },
+          method: 'POST',
+          success: (valid) => {
+            if (valid) {
+              emailValid = true;
+              email.removeClass('is-invalid');
+              if (usernameValid) submit.attr('disabled', false);
+            } else {
+              emailValid = false;
+              email.addClass('is-invalid');
+              submit.attr('disabled', true);
+            }
+          },
+        });
+      } else {
+        emailValid = false;
+        email.addClass('is-invalid');
+        emailInvalidFeedback.text('Enter a valid email address.');
+        submit.attr('disabled', true);
+      }
     }
   });
   emailForm.on('keyup', () => {
