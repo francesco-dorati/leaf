5e49800b38ed22d8a889ebf308d2fd0134449698
Franceco Dorati <fd.francescodorati@gmail.com>
2020-06-08 18:22:09 +0200

    socket fix #5

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 80;
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.set('origins', '*:*');
+io.origins('*:*') // for latest version
 
 // Internal Modules
 const home = require('./src/routes/home')();
