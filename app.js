0adcf31d1f1a810c5ce55c192582f784d3f65fac
Franceco Dorati <fd.francescodorati@gmail.com>
2020-06-08 18:20:18 +0200

    socket fix #4

--- app.js
+++ app.js
@@ -12,17 +12,8 @@ const app = express();
 const port = process.env.PORT || 80;
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-  handlePreflightRequest: (req, res) => {
-      const headers = {
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-          "Access-Control-Allow-Credentials": true
-      };
-      res.writeHead(200, headers);
-      res.end();
-  }
-});
+const io = require('socket.io')(server);
+io.set('origins', '*:*');
 
 // Internal Modules
 const home = require('./src/routes/home')();
